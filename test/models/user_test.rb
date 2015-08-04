fabb7365646f0270d56f08d40135045a14e57584
Tiffany8 <Tiffany8@users.noreply.github.com>
2015-08-04 10:15:23 -0700

    Create test for validation of name attribute

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,10 +3,15 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.name(name: "Example user", email: "user@example.com")
+    @user = User.new(name: "Example user", email: "user@example.com")
   end
 
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "    "
+    assert_not @user.valid?
   end
 end
