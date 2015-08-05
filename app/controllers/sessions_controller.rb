53c48a10df7bbe83082b29cef0832e00f7761fa5
Tiffany8 <Tiffany8@users.noreply.github.com>
2015-08-05 08:24:39 -0700

    Setting up sessions actions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = user.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log the user in and redirect to usre's show page.
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
   end
-  
+
 end
