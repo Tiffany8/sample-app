0c993eb69a4d2885c4075373b7689e411ba51682
Tiffany8 <Tiffany8@users.noreply.github.com>
2015-08-04 23:44:06 -0700

    Add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
 
   get 'signup'  =>  'users#new'
 
+  get 'login' =>    'sessions#new'
+
+  post 'login'  =>  'sessions#create'
+
+  delete 'login'  => 'sessions#destroy'
+
   resources :users
 end
   # The priority is based upon order of creation: first created -> highest priority.
